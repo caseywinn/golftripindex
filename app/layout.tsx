6b5148f9776779a6abaa7173a9bc5671095d1e76
caseywinn <caseywinn@gmail.com>
2026-01-06 17:09:57 +0000

    Update header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,5 @@
 import "./globals.css";
 import SiteHeader from "../components/SiteHeader";
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        <SiteHeader />
-        {children}
-      </body>
-    </html>
-  );
-}
-
 import { Inter_Tight } from "next/font/google";
 
 const interTight = Inter_Tight({
